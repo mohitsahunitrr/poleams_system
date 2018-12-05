ad9b9a4a966dbc39340b266206f2328ef2299185
Philip A Chapman <pchapman@pcsw.us>
2018-12-05 14:09:25 -0800

    Fixed an issue finding the WorkbenchConfig implementation.

---
+++ poleams-workbench/src/main/java/com/precisionhawk/poleams/wb/Main.java
@@ -0,0 +1,13 @@
+package com.precisionhawk.poleams.wb;
+
+/**
+ *
+ * @author pchapman
+ */
+public class Main extends com.precisionhawk.ams.wb.Main {
+
+    public static void main(String[] argsArray) {
+        new Main().execute(argsArray);
+    }
+    
+}
